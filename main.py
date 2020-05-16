61ff5f5cc04dc8e64de23bf5e1ba416cdf83c52c
Stepan Dyatkovskiy <ml@dyatkovskiy.com>
2020-05-16 15:58:32 +0400

    Updated x labels. Added world stats.

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     dataset = Covid19Dataset()
     for r in dataset.regions:
         print(r)
-    dataset.plot(0, dataset.whole_russia_id, show_new=True)
+    dataset.plot_world(0, dataset.world_id, show_new=True)
--- plot.py
+++ plot.py
@@ -29,6 +29,7 @@ def infection_factors_percent(total_cases: List[int]):
 
 def plot_factors(
     offset: int,
+    dates: List[str],
     cases: List[int],
     new_cases: List[int],
     compare_with_new: bool,
@@ -37,23 +38,25 @@ def plot_factors(
 
     # exclude base from indices
     indices = range(offset + 1, offset + len(new_cases))
+    actual_dates = [dates[i] for i in indices]
 
     fig, ax1 = plt.subplots()
 
     color = '#ff0000'
     ax1.set_xlabel('day')
     ax1.set_ylabel('new', color=color)
-    ax1.plot(
-        indices,
+    ax1.plot_russia(
+        actual_dates,
         new_cases[1:] if compare_with_new else cases[1:],
         color=color
     )
     ax1.tick_params(axis='y', labelcolor=color)
+    ax1.tick_params(axis='x', rotation=90)
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
     color = 'tab:blue'
     ax2.set_ylabel('factor %', color=color)  # we already handled the x-label with ax1
-    ax2.plot(indices, factors_function(cases), color=color)
+    ax2.plot_russia(indices, factors_function(cases), color=color)
     ax2.tick_params(axis='y', labelcolor=color)
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
@@ -67,10 +70,18 @@ class Covid19Dataset:
             url: str = 'https://yastat.net/s3/milab/2020/covid19-stat/data/data_struct_1.json'
     ):
         self.json_data = requests.get(url).json()
-        self.dates = self.json_data['russia_stat_struct']['dates']
+        self.russia_dates = self.json_data['russia_stat_struct']['dates']
         self.russia_stat = sorted(
             [
                 value for _, value in self.json_data['russia_stat_struct']['data'].items()
+            ],
+            key=lambda s: s['info']['name']
+        )
+
+        self.world_dates = self.json_data['world_stat_struct']['dates']
+        self.world_stat = sorted(
+            [
+                value for _, value in self.json_data['world_stat_struct']['data'].items()
             ],
             key=lambda s: s['info']['name']
         )
@@ -94,6 +105,16 @@ class Covid19Dataset:
         return regions
 
     @cached_property
+    def world_regions(self) -> List[Region]:
+
+        regions = [
+            Covid19Dataset.Region(i, self.world_stat[i]['info']['name'])
+            for i in range(0, len(self.world_stat))
+        ]
+
+        return regions
+
+    @cached_property
     def whole_russia_id(self) -> int:
         regions = self.regions
         for r in regions:
@@ -101,18 +122,36 @@ class Covid19Dataset:
                 return r.id
         return 0
 
-    def plot(self, base: int, region_id: int, show_new: bool):
+    @cached_property
+    def world_id(self) -> int:
+        regions = self.world_regions
+        for r in regions:
+            if r.name == 'Мир':
+                return r.id
+        return 0
+
+    def plot_russia(self, base: int, region_id: int, show_new: bool):
         total = self.russia_stat[region_id]['cases']
+        dates = self.russia_dates
         new_cases = self.russia_stat[region_id]['cases_delta']
         plot_factors(
             offset=base,
+            dates=dates,
             cases=total[base:],
             new_cases=new_cases[base:],
             factors_function=infection_factors_percent,
             compare_with_new=show_new
         )
 
-
-
-
-
+    def plot_world(self, base: int, region_id: int, show_new: bool):
+        total = self.world_stat[region_id]['cases']
+        dates = self.world_dates
+        new_cases = self.world_stat[region_id]['cases_delta']
+        plot_factors(
+            offset=base,
+            dates=dates,
+            cases=total[base:],
+            new_cases=new_cases[base:],
+            factors_function=infection_factors_percent,
+            compare_with_new=show_new
+        )
